3ab8cbba458751a83726c19097a0e24d774fb569
chadalad <cnuckols7@gmail.com>
2020-07-06 18:57:45 -0400

    july 6 stretch

--- redux/actions.js
+++ redux/actions.js
@@ -2,6 +2,11 @@ const { spookyTypes } = require('./types');
 
 //code actions here
 
+const incrementStep = ({ value }) => ({
+  type: spookyTypes.GET_STEPS,
+  value,
+})
+
 module.exports = {
   incrementStep,
   createNewSpookySound,
--- redux/types.js
+++ redux/types.js
@@ -1,5 +1,8 @@
 const spookyTypes = {
   //code types here
+  GET_STEPS: 'GET_STEPS',
+  SET_STEPS: 'SET_STEPS',
+  SET_SOUND: 'SET_SOUND',
 };
 
 module.exports = { spookyTypes };
